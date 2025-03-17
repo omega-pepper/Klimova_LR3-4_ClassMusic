124d8bacc987d43ab5681dd47e5ed9a2e63fefb4
Julia <cimafor2003@mail.ru>
2025-03-17 10:49:10 +0300

    started to writing

--- Klimova_LR3-4_Main.cpp
+++ Klimova_LR3-4_Main.cpp
@@ -1,3 +1,5 @@
+#include "Klimova_LR3-4_ClassMusic.h"
+#include "Klimova_LR3-4_ClassMusic.cpp"
 #include "Klimova_LR3-4_Methods.h"
 
 // Функция для демонстрации всех конструкторов
@@ -15,24 +17,24 @@ void demoConstructors() {
 
 // Функция для демонстрации работы методов
 void demoMethods() {
-    Composition comp("Example Song", "Example Author", 4.2, {"2022"});
-    cout << "Original composition:\n" << comp << endl;
+    Composition comp("Пример песенки", "Пример автора", 4.2, {"2022"});
+    cout << "Оригинальная композиция: " << comp << endl;
     
-    comp.setTitle("New Title");
-    comp.setAuthor("New Author");
+    comp.setTitle("Новое название");
+    comp.setAuthor("Новый автор");
     comp.setDuration(5.0);
     comp.setReleases({"2023", "2024"});
     
-    cout << "Updated composition:\n" << comp << endl;
+    cout << "Обновлённая композиция: " << comp << endl;
 }
 
 // Функция демонстрации перегруженных операций
 void demoOperators() {
-    Composition comp1("Song X", "Artist X", 3.0, {"2021"});
-    Composition comp2("Song Y", "Artist Y", 4.5, {"2022"});
+    Composition comp1("Песенка по оси икс", "Артист по оси икс", 3.0, {"2021"});
+    Composition comp2("Песенка по оси игрек", "Артист по оси игрек", 4.5, {"2022"});
     
-    cout << "Comparison (comp1 < comp2): " << (comp1 < comp2 ? "True" : "False") << endl;
-    cout << "Addition (comp1 + comp2):\n" << (comp1 + comp2) << endl;
+    cout << "Сравнение (Песенка икс < Песенка игрек): " << (comp1 < comp2 ? "Ну тут всё кристально ясно" : "Пасмурно") << endl;
+    cout << "Прибавка (Песенка икс + Песенка игрек):\n" << (comp1 + comp2) << endl;
     
     cout << "Prefix Increment:\n" << ++comp1 << endl;
     cout << "Postfix Increment:\n" << comp1++ << endl;
@@ -41,22 +43,25 @@ void demoOperators() {
 
 // Функция демонстрации работы с массивом объектов
 void demoArray() {
-    compositions.push_back(Composition("Track 1", "Artist 1", 2.5, {"2019"}));
-    compositions.push_back(Composition("Track 2", "Artist 2", 3.8, {"2020"}));
-    compositions.push_back(Composition("Track 3", "Artist 3", 4.1, {"2021"}));
+    compositions.push_back(Composition("Крутой трек", "Крутой артист", 2.5, {"2019"}));
+    compositions.push_back(Composition("Не оч крутой трек", "Не оч крутой артист", 3.8, {"2020"}));
+    compositions.push_back(Composition("Каддилак", "Моргенштерн", 4.1, {"2021"}));
     displayAllCompositions();
 }
 
 // Функция демонстрации сортировки
 void demoSorting() {
     vector<Composition> sorted = getSortedByDuration();
-    cout << "Sorted compositions by duration:\n";
+    cout << "Компоцзиции осортированы по длительности:\n";
     for (const auto& comp : sorted) {
         cout << comp << endl;
     }
 }
 
 int main() {
+
+    setlocale(LC_ALL, "Russian");
+
     map<int, void(*)()> menu = {
         {1, demoConstructors},
         {2, demoMethods},
@@ -67,15 +72,15 @@ int main() {
     
     while (true) {
         cout << "\nMenu:\n";
-        cout << "1. Demonstrate Constructors\n";
-        cout << "2. Demonstrate Methods\n";
-        cout << "3. Demonstrate Operators\n";
-        cout << "4. Demonstrate Array of Compositions\n";
-        cout << "5. Demonstrate Sorting\n";
-        cout << "0. Exit\n";
+        cout << "1. Конструкторы.\n";
+        cout << "2. Методы.\n";
+        cout << "3. Операторы.\n";
+        cout << "4. Список композиций (массив).\n";
+        cout << "5. Сортировка по длительности.\n";
+        cout << "0. Выход.\n";
         
         int choice;
-        cout << "Enter choice: ";
+        cout << "Введите число: ";
         cin >> choice;
         
         if (choice == 0) break;
@@ -84,7 +89,7 @@ int main() {
         if (it != menu.end()) {
             it->second();
         } else {
-            cout << "Invalid choice!\n";
+            cout << "Фигня!\n";
         }
     }
     
